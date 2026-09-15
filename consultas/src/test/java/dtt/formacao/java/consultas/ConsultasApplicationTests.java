d2a36a125e9ae5856095c9b91f681e5359363c83
Leonor Pinto <leopinto@deloitte.pt>
2026-09-15 11:42:07 +0100

    added spring folder

---
+++ consultas/src/test/java/dtt/formacao/java/consultas/ConsultasApplicationTests.java
@@ -0,0 +1,13 @@
+package dtt.formacao.java.consultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
