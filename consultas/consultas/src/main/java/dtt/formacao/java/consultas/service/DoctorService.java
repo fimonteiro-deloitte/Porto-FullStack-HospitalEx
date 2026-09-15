4780ba3a734b32ce368ede2810cee813e4c268be
joaopedcosta <joaopedcosta@deloitte.pt>
2026-09-15 15:21:47 +0100

    feat: Doctor Service Updated.

--- consultas/consultas/src/main/java/dtt/formacao/java/consultas/service/DoctorService.java
+++ consultas/consultas/src/main/java/dtt/formacao/java/consultas/service/DoctorService.java
@@ -1,21 +1,41 @@
 package dtt.formacao.java.consultas.service;
 
 import dtt.formacao.java.consultas.model.Doctor;
+import dtt.formacao.java.consultas.repository.DoctorRepository;
 import dtt.formacao.java.consultas.model.refs.Specialty;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public interface DoctorService {
+public class DoctorService {
 
-    Doctor createDoctor(Doctor doctor);
+    private final DoctorRepository doctorRepository;
 
-    Doctor updateDoctor(Long id, Doctor doctor);
+    public DoctorService(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
 
-    Doctor getDoctorById(Long id);
+    public Doctor createDoctor(Doctor doctor) {
+        return doctorRepository.save(doctor);
+    }
 
-    List<Doctor> getAllDoctors();
+    public Doctor updateDoctor(Long id, Doctor doctor) {
+        doctor.setId(id);
+        return doctorRepository.save(doctor);
+    }
 
-    List<Doctor> getDoctorsBySpecialty(Specialty specialty);
+    public List<Doctor> getAllDoctors() {
+        return doctorRepository.findAll();
+    }
+
+    public Doctor getDoctorById(Long id) {
+        return doctorRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Doctor not found"));
+    }
+
+    public List<Doctor> getDoctorsBySpecialty(Specialty specialty) {
+        return doctorRepository.findBySpecialty(specialty);
+    }
 }
